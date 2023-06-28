0fec131a08d4ac2a30af1aabfb0ff308fcc8978d
vthrive admin <vthrive@vthrives-MacBook-Pro.local>
2023-06-27 23:56:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+
+// 1st way - from json file inside src
+import userjsonData from "./local-json/users.json";
+
+// 2nd way - from js file inside src
+import { usersjsExpArrayData } from "./local-json/users.js";  // makesure you have this variable "usersjsExpArrayData" in Users.js file
+
+export default function App() {
+   //1. const and methods
+   //2. useEffect( call methods );
+   //3. return( <div> </div> );
+
+  const [usersjson, setUsers] = useState([]);
+  const [usersjsExp, setUsersJS] = useState([]);
+  const [usersaxios, setUsersAxios] = useState([]);
+  const [usersaxiosEXT, setUsersAxiosEXT] = useState([]);
+
+  useEffect(() => 
+  {
+    setUsers(userjsonData);
+    setUsersJS(usersjsExpArrayData);
+
+    // 3rd way - uisng axios to load the file
+    axios
+    .get("./data.json")
+    .then((res) => setUsersAxios(res.data))
+    .catch(err=>console.log(err))
+
+      // 4rd way - uisng axios to load EXTERENAL file  
+      //open a new terminal
+      //> npm install -g json-server
+      //> json-server --watch src/local-json/usersjsonsrv.json --port 3009
+      axios
+      .get("http://localhost:3009/users")
+      .then((res) => setUsersAxiosEXT(res.data))
+      .catch(
+          err=>console.log(err)
+          )
+  
+
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src= {require('./silverleaf.jpg')} className="App-logo" background-color="transparent"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch data in different ways </h1>
+      <hr />
+      <div className="title-text"><strong>users</strong>(<i>JSON data from users.json file inside "src/local-json" folder</i>)</div>
+      {usersjson &&
+        usersjson?.map(({ name, id }) => (
+          <div key={id} className="list-row">
+            <strong>{name}</strong>
+          </div>
+        ))}
+
+        <hr />
+
+        <div className="title-text"><strong>users</strong> (<i>JSON Exported data from users.js file inside "src/local-json" folder</i>)</div>
+        {usersjsExp &&
+        usersjsExp?.map(({ name, id }) => (
+          <div key={id} className="list-row">
+            <strong>{name}</strong>
+          </div>
+        ))}
+
+        <hr />
+
+        <div className="title-text"><strong>users</strong> (<i>JSON data from data.json inside public folder using react API call(fetch / axios)</i>)</div>
+        {usersaxios &&
+        usersaxios?.map(({ name, id }) => (
+          <div key={id} className="list-row">
+            <strong>{name}</strong>
+          </div>
+        ))}
+
+        <hr />
+
+        <div className="title-text"><strong>users</strong> (<i>JSON data from users.json running in external host and consuming using react API call(fetch / axios)</i>)</div>
+        {usersaxiosEXT &&
+        usersaxiosEXT?.map(({ name, id }) => (
+          <div key={id} className="list-row">
+            <strong>{name}</strong>
+          </div>
+        ))}
+
+       <hr />
     </div>
   );
 }
-
-export default App;
---
+++ src/local-json/users.js
@@ -0,0 +1,22 @@
+import { v4 as uuid } from 'uuid';
+
+export let usersjsExpArrayData = [
+    {
+        id: uuid(),
+        "name": "Local User3",
+        "nickname": "SL4",
+        "email": "SL4@gmail.com"
+      },
+      {
+        id: uuid(),
+        "name": "Local User4",
+        "nickname": "SL5",
+        "email": "SL5@gmail.com"
+      },
+      {
+        id: uuid(),
+        "name": "Local User5",
+        "nickname": "SL6",
+        "email": "SL6@gmail.com"
+      }
+];
